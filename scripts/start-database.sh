c89e8b2cc1440ef2dde934ebd3eab330e5e419ec
Artur Skonecki <skoneka@users.noreply.github.com>
2015-09-24 22:57:10 +0200

    mongod --smallfiles --dbpath /var/pryv/data/mongodb

--- scripts/start-database.sh
+++ scripts/start-database.sh
@@ -6,7 +6,7 @@ cd $SCRIPT_FOLDER/..
 
 
 if [ `uname` = "Linux" ]; then
-mongod --smallfiles --dbpath ../mongodb-data
+mongod --smallfiles --dbpath /var/pryv/data/mongodb
 else
 ../mongodb-osx-x86_64-2.6.0/bin/mongod --smallfiles --dbpath ../mongodb-data
 fi
