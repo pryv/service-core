a2c9a07d7543417d4130b8de90e7d07fea277b0e
ikebets <ilia.kebets@epfl.ch>
2016-10-04 13:54:33 +0200

    initial test cases

---
+++ components/api-server/test/blockchain.test.js
@@ -0,0 +1,53 @@
+/*global describe, it */
+
+describe('Blockchain', function () {
+
+
+  describe('Events', function () {
+
+    describe('GET', function () {
+
+
+
+    });
+
+    describe('CREATE', function () {
+
+      it('should not return a blockchain field when the server setting is off', function () {
+
+      });
+
+      it('should return a blockchain when the server setting is on', function () {
+
+      });
+
+    });
+
+
+    describe('UPDATE', function () {
+
+      it('should not return a blockchain field when the server setting is off', function () {
+
+      });
+
+      it('should return a blockchain when the server setting is on', function () {
+
+      });
+
+    });
+
+    describe('DELETE', function () {
+
+      it('should not return a blockchain field when the server setting is off', function () {
+
+      });
+
+      it('should return a blockchain when the server setting is on', function () {
+
+      });
+
+    });
+
+
+  });
+});
