eccb93928221e0a27ce98052c369e9716940e5fe
Kaspar Schiess <kaspar.schiess@technologyastronauts.ch>
2017-01-18 14:36:48 +0100

    Clean up log creation

--- build/service-core/release.sh
+++ build/service-core/release.sh
@@ -4,6 +4,8 @@ source /pd_build/buildconfig
 
 service_name="service-core"
 target_dir="/var/src/$service_name"
+log_dir="/var/pryv/data/log"
+log_file="$log_dir/api-server.errors.log"
 
 header "Install application from release.tar"
 
@@ -20,6 +22,10 @@ PYTHON=$(which python2.7) run npm install --production
 
 # Install the config file
 run cp /pd_build/config/service-core.json $target_dir/default.json
+
+# Create the log
+run mkdir -p $log_dir && touch $log_file && \
+  chown app:app $log_file
 
 # Install the script that runs the nodejs service
 run mkdir /etc/service/$service_name
