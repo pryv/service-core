6ec8a45860be3e2a29b3b8996353ac5336f0d192
Kaspar Schiess <kaspar@pryv.com>
2017-03-10 18:31:00 +0100

    Add type checks
    Where ever we treat values in req.body as a string, we need to check if we really have a string like
    object in front of us.

--- source/middleware/src/override.js
+++ source/middleware/src/override.js
@@ -1,33 +1,39 @@
+'use strict';
+// @flow
+
 var APIError = require('components/errors').APIError,
     ErrorIds = require('components/errors').ErrorIds;
 
 /**
- * Middleware to allow overriding HTTP method, "Authorization" header and JSON body content
- * by sending them as fields in urlencoded requests.
- * Does not perform request body parsing (expects req.body to exist), so must be executed after
- * e.g. bodyParser middleware.
+ * Middleware to allow overriding HTTP method, "Authorization" header and JSON
+ * body content by sending them as fields in urlencoded requests. Does not
+ * perform request body parsing (expects req.body to exist), so must be executed
+ * after e.g. bodyParser middleware.
  */
-module.exports = function (req, res, next) {
+module.exports = function (req: express$Request, res: express$Response, next: () => void) {
   if (! req.is('application/x-www-form-urlencoded')) { return next(); }
+  
+  const body = req.body; 
+  if (body == null || typeof body !== 'object') return next();
 
-  if (req.body._method) {
+  if (typeof body._method == 'string') {
     req.originalMethod = req.originalMethod || req.method;
-    req.method = req.body._method.toUpperCase();
-    delete req.body._method;
+    req.method = body._method.toUpperCase();
+    delete body._method;
   }
 
-  if (req.body._auth) {
+  if (body._auth) {
     if (req.headers.authorization) {
       req.headers['original-authorization'] = req.headers.authorization;
     }
-    req.headers.authorization = req.body._auth;
-    delete req.body._auth;
+    req.headers.authorization = body._auth;
+    delete body._auth;
   }
 
-  if (req.body._json) {
-    req.originalBody = req.originalBody || req.body;
+  if (typeof body._json === 'string') {
+    req.originalBody = req.originalBody || body;
     try {
-      req.body = JSON.parse(req.body._json);
+      req.body = JSON.parse(body._json);
     } catch (err) {
       return next(new APIError(ErrorIds.InvalidRequestStructure, err.message, {httpStatus: 400}));
     }
