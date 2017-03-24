ad9a88a9cd273f6b76816a1ca80c8b9f0fdf361a
Kaspar Schiess <kaspar@pryv.com>
2017-03-24 11:46:21 +0100

    Refactor test

--- source/business/test/unit/series/repository.test.js
+++ source/business/test/unit/series/repository.test.js
@@ -11,22 +11,24 @@ const Repository = series.Repository;
 const DataMatrix = series.DataMatrix; 
 
 describe('business.series.Repository', function () {
-  const namespace = 'pryv-userdb.USER_ID';  // influx database
-  const seriesName = 'event.EVENT_ID';      // influx measurement
-  const data = new DataMatrix(); 
+  describe('with stubbed out connection', function () {
+    const namespace = 'pryv-userdb.USER_ID';  // influx database
+    const seriesName = 'event.EVENT_ID';      // influx measurement
+    const data = new DataMatrix(); 
 
-  // A test double for the actual connection:
-  const influxConnection = {
-    ensureDatabase: () => Promise.resolve(true),
-    writeMeasurement: () => Promise.resolve(true), 
-  };
+    // A test double for the actual connection:
+    const influxConnection = {
+      ensureDatabase: () => Promise.resolve(true),
+      writeMeasurement: () => Promise.resolve(true), 
+    };
 
-  it('should produce series objects for events', function () {
-    const repository = new Repository(influxConnection);
-    const series = repository.get(namespace, seriesName);
-    
-    return series
-      .then((series) => series.append(data)); 
+    it('should produce series objects for events', function () {
+      const repository = new Repository(influxConnection);
+      const series = repository.get(namespace, seriesName);
+      
+      return series
+        .then((series) => series.append(data)); 
+    });
   });
 });
 
