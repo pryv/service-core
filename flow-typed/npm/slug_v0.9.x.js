6491f14e65953d769af6728bfcd5dc8ac2bbf872
Kaspar Schiess <kaspar@pryv.com>
2017-09-29 08:41:20 +0200

    Updating flow defs

--- flow-typed/npm/slug_v0.9.x.js
+++ flow-typed/npm/slug_v0.9.x.js
@@ -1,17 +1,17 @@
-// flow-typed signature: 5d1365421f3793af8b0e98a1995f79b4
-// flow-typed version: 7154c86e07/slug_v0.9.x/flow_>=v0.28.x
+// flow-typed signature: c499686d8ed4b3da5bd13133389c6088
+// flow-typed version: b43dff3e0e/slug_v0.9.x/flow_>=v0.25.x
 
 type SlugMode = 'rfc3986' | 'pretty'
 
 declare module 'slug' {
   declare type SlugOptions = {
-    mode: SlugMode,
-    replacement: string,
-    multicharmap: { [key: string]: string },
-    charmap: { [key: string]: string },
-    remove: RegExp,
-    lower: boolean,
-    symbol: boolean,
+    mode?: SlugMode,
+    replacement?: string,
+    multicharmap?: { [key: string]: string },
+    charmap?: { [key: string]: string },
+    remove?: ?RegExp,
+    lower?: boolean,
+    symbols?: boolean,
   }
   declare module.exports: {
       (input: string, optionOrReplacement?: string | SlugOptions): string,
