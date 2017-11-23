9b0698e8d1d8009cb4190f4f60ba25cf17cb63df
Thiébaud Modoux <tmodoux@pryv.com>
2017-11-23 15:20:27 +0100

    Augment forbidden-updates test to cover separately each protected field (for Events,Streams and Accesses)

--- components/api-server/test/forbidden-updates.test.js
+++ components/api-server/test/forbidden-updates.test.js
@@ -2,49 +2,94 @@
 
 const commonFns = require('../src/methods/helpers/commonFunctions');
 const streamSchema = require('../src/schema/stream');
+const eventsSchema = require('../src/schema/event');
+const accessesSchema = require('../src/schema/access');
 const should = require('should');
+const async = require('async');
 
 describe('methods/helpers/commonFunctions.js: catchForbiddenUpdate(schema)', function () {
-  const forbiddenUpdate = {
-    update: {
-      id: 'forbidden',
-      modifiedBy: 'toto'
-    }
-  };
-
-  it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is null', function (done) {
-    testForbiddenUpdate(forbiddenUpdate, null, done);
-  });
   
-  it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is false', function (done) {
-    testForbiddenUpdate(forbiddenUpdate, false, done);
-  });
-  
-  it('must not throw any error if "ignoreProtectedFieldUpdates" is true but print a warn log', function (done) {
-    testForbiddenUpdate(forbiddenUpdate, true, done);
-  });
-  
-  function testForbiddenUpdate(update, ignoreProtectedFieldUpdates, done) {
-    // Here we fake a logger to test that a warning is logged in non-strict mode
-    const logger = {
-      warn: function(msg) {
-        should(ignoreProtectedFieldUpdates).be.true();
-        should(msg.indexOf('Forbidden update was attempted on the following protected field(s)') >= 0).be.true();
-        should(msg.indexOf('Server has "ignoreProtectedFieldUpdates" turned on: Fields are not updated, but no error is thrown.') >= 0).be.true();
-        should(msg.indexOf('id') >= 0).be.true();
-        should(msg.indexOf('modifiedBy') >= 0).be.true();
-        done();
-      }
-    };
-    const catchForbiddenUpdate = commonFns.catchForbiddenUpdate(streamSchema('update'), ignoreProtectedFieldUpdates, logger);
-    catchForbiddenUpdate(null, forbiddenUpdate, null, function(err) {
-      if(!ignoreProtectedFieldUpdates) {
-        should.exist(err);
-        done();
-      } else if(err) {
-        done(err);
-      }
+  describe('streams schema', function () {
+    
+    const protectedFields = ['id', 'children', 'created', 'createdBy', 'modified', 'modifiedBy'];
+    
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is null', function (done) {
+      testForbiddenUpdate(streamSchema, protectedFields, null, done);
     });
+    
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is false', function (done) {
+      testForbiddenUpdate(streamSchema, protectedFields, false, done);
+    });
+    
+    it('must not throw any error if "ignoreProtectedFieldUpdates" is true but print a warn log', function (done) {
+      testForbiddenUpdate(streamSchema, protectedFields, true, done);
+    });
+  });
+  
+  describe('events schema', function () {
+    
+    const protectedFields = ['id', 'attachments', 'created', 'createdBy', 'modified', 'modifiedBy'];
+
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is null', function (done) {
+      testForbiddenUpdate(eventsSchema, protectedFields, null, done);
+    });
+    
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is false', function (done) {
+      testForbiddenUpdate(eventsSchema, protectedFields, false, done);
+    });
+    
+    it('must not throw any error if "ignoreProtectedFieldUpdates" is true but print a warn log', function (done) {
+      testForbiddenUpdate(eventsSchema, protectedFields, true, done);
+    });
+  });
+  
+  describe('accesses schema', function () {
+    
+    const protectedFields = ['id', 'token', 'type', 'lastUsed', 'created', 'createdBy', 'modified', 'modifiedBy'];
+
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is null', function (done) {
+      testForbiddenUpdate(accessesSchema, protectedFields, null, done);
+    });
+    
+    it('must throw a forbidden error if "ignoreProtectedFieldUpdates" is false', function (done) {
+      testForbiddenUpdate(accessesSchema, protectedFields, false, done);
+    });
+    
+    it('must not throw any error if "ignoreProtectedFieldUpdates" is true but print a warn log', function (done) {
+      testForbiddenUpdate(accessesSchema, protectedFields, true, done);
+    });
+  });
+  
+  function testForbiddenUpdate(schema, protectedFields, ignoreProtectedFieldUpdates, done) {
+    async.eachSeries(
+      protectedFields,
+      function testForbiddenUpdateForEachField(protectedField, stepDone) {
+        // Here we fake a logger to test that a warning is logged in non-strict mode
+        const logger = {
+          warn: function(msg) {
+            should(ignoreProtectedFieldUpdates).be.true();
+            should(msg.indexOf('Forbidden update was attempted on the following protected field(s)') >= 0).be.true();
+            should(msg.indexOf('Server has "ignoreProtectedFieldUpdates" turned on: Fields are not updated, but no error is thrown.') >= 0).be.true();
+            should(msg.indexOf(protectedField) >= 0).be.true();
+            stepDone();
+          }
+        };
+        const catchForbiddenUpdate = commonFns.catchForbiddenUpdate(schema('update'), ignoreProtectedFieldUpdates, logger);
+        let forbiddenUpdate = {update: {}};
+        forbiddenUpdate.update[protectedField] = 'forbidden';
+        catchForbiddenUpdate(null, forbiddenUpdate, null, function(err) {
+          if(!ignoreProtectedFieldUpdates) {
+            should.exist(err);
+            should(err.id).be.equal('forbidden');
+            should(err.httpStatus).be.equal(403);
+            stepDone();
+          } else if(err) {
+            stepDone(err);
+          }
+        });
+      },
+      done
+    );
   }
   
 });
