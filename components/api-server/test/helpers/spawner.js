ae7e58e1b82e77743458a99e7e3569fb7e08eabe
Kaspar Schiess <kaspar@pryv.com>
2017-12-11 10:31:41 +0100

    Extracts ConditionVariabel and Fuse from spawner.js

--- components/api-server/test/helpers/spawner.js
+++ components/api-server/test/helpers/spawner.js
@@ -1,10 +1,11 @@
 // @flow
 
-const bluebird = require('bluebird');
 const lodash = require('lodash');
 const url = require('url');
 const child_process = require('child_process');
 const msgpack = require('msgpack5')();
+
+const { ConditionVariable, Fuse } = require('./condition_variable');
 
 // Set DEBUG=spawner to see these messages.
 const debug = require('debug')('spawner');
@@ -267,98 +268,6 @@ class Server {
     return new url.URL(path || '', this.baseUrl).toString();
   }
 }
-
-class Waiter {
-  promise: Promise<void>;
-  resolve: () => void; 
-  reject: (err: Error) => void; 
-  timeout: ?number;
-  
-  done: boolean; 
-  
-  constructor(timeout?: number) {
-    this.done = false; 
-    
-    this.promise = new bluebird((res, rej) => {
-      this.resolve = res; 
-      this.reject = rej;
-      
-      if (timeout != null && timeout > 0) {
-        this.timeout = setTimeout(() => this.timeoutFired(), timeout);
-      }
-    });
-  }
-  
-  timeoutFired() {
-    this.reject(
-      new Error('timeout'));
-  }
-  
-  release() {
-    if (this.done) throw new Error('AF: waiter is not released');
-    this.done = true; 
-    
-    this.resolve(); 
-  }
-}
-class ConditionVariable {
-  waiters: Array<Waiter>;
-  
-  constructor() {
-    this.waiters = []; 
-  }
-  
-  wait(timeout?: number): Promise<void> {
-    const waiter = new Waiter(timeout);
-      
-    this.waiters.push(waiter);
-    
-    return waiter.promise; 
-  }
-  
-  broadcast() {
-    const list = this.waiters; 
-    
-    // release this reference before broadcasting; this avoids somehow
-    // broadcasting twice during the first broadcast. 
-    this.waiters = []; 
-    
-    for (const waiter of list) {
-      waiter.release(); 
-    }
-  }
-}
-
-// A fuse that can be burnt once. As long as it is not burnt, waiters can
-// register to be nofitied when the fuse burns. Once burnt, it always notifies
-// immediately. Like a combination of a boolean and a ConditionVariable.
-//
-class Fuse {
-  cv: ConditionVariable; 
-  burnt: boolean; 
-  
-  constructor() {
-    this.burnt = false; 
-    this.cv = new ConditionVariable(); 
-  }
-  
-  async wait(timeout?: number): Promise<void> {
-    if (this.burnt) return; 
-    
-    await this.cv.wait(timeout); 
-    return;
-  }
-  
-  burn() {
-    this.burnt = true; 
-    this.cv.broadcast();
-  }
-  
-  isBurnt(): boolean {
-    return this.burnt;
-  }
-}
-
 
 module.exports = {
   SpawnContext: SpawnContext,
