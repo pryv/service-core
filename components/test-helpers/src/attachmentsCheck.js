0a412a67d6dd7123a7693e6fa17bd6395ab16186
Kaspar Schiess <kaspar@pryv.com>
2018-02-16 08:38:32 +0100

    @flow the attachmentsCheck helper

--- components/test-helpers/src/attachmentsCheck.js
+++ components/test-helpers/src/attachmentsCheck.js
@@ -1,3 +1,5 @@
+// @flow
+
 /**
  * Test helper functions for attached files.
  */
@@ -7,22 +9,19 @@ var childProcess = require('child_process'),
     testData = require('./data'),
     eventFilesStorage = require('./dependencies').storage.user.eventFiles;
 
-/**
- * Returns an empty string if the tested file attached to the specified event is identical to the
- * original file. (Runs command-line util `cmp` underneath.)
- *
- * @param {Object} user
- * @param {String} eventId
- * @param {String} fileId
- * @param {String} originalFileName The name of the original file (in test data), if different from
- *                                  fileName. Optional.
- */
-exports.compareTestAndAttachedFiles = function (user, eventId, fileId, originalFileName) {
-  if (! originalFileName) {
+// Returns an empty string if the tested file attached to the specified event
+// is identical to the original file. (Runs command-line util `cmp`
+// underneath).
+// 
+exports.compareTestAndAttachedFiles = function (
+  user: mixed, eventId: string, fileId: string, originalFileName: ?string) 
+{
+  if (originalFileName == null) {
     originalFileName = fileId;
   }
-  return cmp(eventFilesStorage.getAttachedFilePath(user, eventId, fileId),
-             path.join(testData.attachmentsDirPath, originalFileName));
+  return cmp(
+    eventFilesStorage.getAttachedFilePath(user, eventId, fileId),
+    path.join(testData.attachmentsDirPath, originalFileName));
 };
 
 function cmp(filePathA, filePathB) {
