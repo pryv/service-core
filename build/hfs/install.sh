1f2b9a07528eaa6a32cea57d48c6c4d236766972
Thiébaud Modoux <tmodoux@pryv.com>
2018-02-21 16:25:34 +0100

    Do not remove unused default services but deactivate them instead through docker build config

--- build/hfs/install.sh
+++ build/hfs/install.sh
@@ -5,9 +5,5 @@ source /pd_build/buildconfig
 # Install the application.
 run /pd_build/release.sh
 
-# Remove cron and sshd entirely, unless we use them
-run rm -r /etc/service/sshd && rm /etc/my_init.d/00_regen_ssh_host_keys.sh
-run rm -r /etc/service/cron
-
 # Clean up after ourselves.
 run /pd_build/finalize.sh
