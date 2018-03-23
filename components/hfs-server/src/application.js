ee9d046f54c26a7dd1ae5bcf44e15a0dd2ba02c2
Kaspar Schiess <kaspar@pryv.com>
2018-03-23 08:39:50 +0100

    Adds a neutral 'metadata updater' service implementation
    This will serve as a way to loosen the dependency during development or
    even production.

--- components/hfs-server/src/application.js
+++ components/hfs-server/src/application.js
@@ -10,7 +10,8 @@ const Context = require('./context');
 const Settings = require('./Settings');
 const Server = require('./server'); 
 
-import type {LogFactory} from 'components/utils/src/logging';
+import type { LogFactory, Logger } from 'components/utils/src/logging';
+import type { IMetadataUpdaterService, IUpdateResponse } from 'components/metadata';
 
 // const { Tags } = require('opentracing');
 const opentracing = require('opentracing');
@@ -53,6 +54,9 @@ function createContext(settings: Settings, logFactory: LogFactory): Context {
     
   return new Context(influx, mongo, logFactory('model'), tracer, typeRepoUpdateUrl);
 }
+
+// Produce a tracer that allows creating span trees for a subset of all calls. 
+// 
 function produceTracer(settings, logger) {
   if (! settings.get('trace.enable').bool()) 
     return new opentracing.Tracer();
@@ -83,19 +87,30 @@ function patchMongoDBDriver(tracer) {
   patch(tracer);
 }
 
-/** The HF application holds references to all subsystems and ties everything
- * together. 
- */
+// Produces an instance of Metadata#MetadataUpdaterService which will allow 
+// updating events in MongoDB out of band. 
+// 
+function produceMetadataUpdater(settings: Settings, logger: Logger): IMetadataUpdaterService {
+  return new MetadataForgetter(logger); 
+}
+
+// The HF application holds references to all subsystems and ties everything
+// together. 
+// 
 class Application {
   settings: Settings; 
   logFactory: LogFactory; 
   context: Context; 
   
+  metadataUpdater: IMetadataUpdaterService;
+  
   server: Server; 
   
   init(settings?: Settings): Application {
     this.settings = settings || createSettings(); 
-    this.logFactory = createLogFactory(this.settings);
+    const lf = this.logFactory = createLogFactory(this.settings);
+    this.metadataUpdater = produceMetadataUpdater(this.settings, lf('metadata-updater'));
+    
     this.context = createContext(this.settings, this.logFactory);
 
     this.server = new Server(this.settings, this.context);
@@ -112,6 +127,30 @@ class Application {
   run() {
     this.init(); 
     this.start(); 
+  }
+}
+
+// A null object that implements the MetadataUpdaterService interface; if no
+// connection to an updater is configured in the configuration file, this will
+// be used - and no updates will be made. 
+// 
+class MetadataForgetter implements IMetadataUpdaterService {
+  logger: Logger; 
+  
+  constructor(logger: Logger) {
+    this.logger = logger; 
+  }
+  
+  async scheduleUpdate(/* req: IUpdateRequest */): Promise<IUpdateResponse> {
+    const logger = this.logger; 
+    
+    logger.warn('Metadata of events will NOT be updated; please configure the metadata update service.');
+    
+    // Returns the present instant as a deadline, since the operation is 
+    // considered complete. 
+    return {
+      deadline: new Date() * 1e9,
+    };
   }
 }
 
---
+++ components/metadata/src/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+// Exports from the metadata package; mostly interfaces that the rpc server
+// implements for construction of rpc clients. 
+
+import { IMetadataUpdaterService, IUpdateRequest, IUpdateResponse } from './metadata_updater/interface';
+export type { IMetadataUpdaterService, IUpdateRequest, IUpdateResponse };
