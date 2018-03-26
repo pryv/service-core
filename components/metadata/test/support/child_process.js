eee3f558c1e5b73815be12507bf0119be078564b
Kaspar Schiess <kaspar@pryv.com>
2018-03-26 14:16:54 +0200

    A child process launcher stub

--- components/metadata/test/support/child_process.js
+++ components/metadata/test/support/child_process.js
@@ -1,1 +1,17 @@
+// @flow
 
+const debug = require('debug')('child_process');
+
+const ChildProcess = require('components/test-helpers').child_process;
+
+class ApplicationLauncher {
+  constructor() {
+  }
+  
+  launch(injectSettings: {}) {
+  }
+}
+
+const appLauncher = new ApplicationLauncher(); 
+const childProcess = new ChildProcess(appLauncher);
+childProcess.run();
