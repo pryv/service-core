219760bacad8e414868024a1e4a5a29257dce513
Kaspar Schiess <kaspar@pryv.com>
2018-04-27 13:35:08 +0200

    Adds more type information

--- components/middleware/src/commonHeaders.js
+++ components/middleware/src/commonHeaders.js
@@ -3,11 +3,15 @@
 // Middleware to handle OPTIONS requests and to add CORS headers to all other 
 // requests. 
 
+type ServerInfo = {
+  version: string
+}
+
 /**
  * @param serverInfo Must contain `version`
  * @return {Function}
  */
-module.exports = function (serverInfo) {
+module.exports = function (serverInfo: ServerInfo) {
   return function (req: express$Request, res: express$Response, next: express$NextFunction) {
     // allow cross-domain requests (CORS)
     res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
