b2b5ca4de5c941d222f4f4829a7680331c777f68
Kaspar Schiess <kaspar@pryv.com>
2018-05-07 15:28:45 +0200

    Outside of git, use this static file
    Build process compiles the current version into '.api-version'. Then we
    use that file to determine the version returned to clients on boot.

--- components/middleware/test/unit/project_version.test.js
+++ components/middleware/test/unit/project_version.test.js
@@ -1,12 +1,14 @@
 // @flow
 
-/* global describe, it, beforeEach */
+/* global describe, it, beforeEach, afterEach */
 
 const chai = require('chai');
 const assert = chai.assert; 
 
 const bluebird = require('bluebird');
 const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 const { ProjectVersion } = require('../../src/project_version');
 
@@ -16,7 +18,7 @@ describe('ProjectVersion#version', () => {
     pv = new ProjectVersion(); 
   });
   
-  describe('when no ".deploy" file is available', () => {
+  describe('when no ".api-version" file is available', () => {
     it('returns a version-string', async () => {
       const version = await pv.version();
       assert.match(version, /^\d+\.\d+\.\d+(-\d+-[0-9a-z]+)?$/);
@@ -30,6 +32,20 @@ describe('ProjectVersion#version', () => {
       const given = await pv.version();
       
       assert.strictEqual(given, expected);
+    });
+  });
+  describe('when a ".api-version" file exists in the project', () => {
+    const versionFilePath = path.join(__dirname, '../../../../../', '.api-version');
+
+    beforeEach(() => {
+      fs.writeFileSync(versionFilePath, '1.2.3');      
+    });
+    afterEach(() => {
+      fs.unlinkSync(versionFilePath);
+    });
+    
+    it('reads .api-version and returns that constant', async () => {
+      assert.strictEqual(await pv.version(), '1.2.3');
     });
   });
 });
