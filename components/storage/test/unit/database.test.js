ef3bcda795dc85536f5be119179028e8d83bd0b7
Thiébaud Modoux <tmodoux@pryv.com>
2019-03-14 16:47:33 +0100

    Add unit test for Database.isDuplicateError.

--- components/storage/test/unit/database.test.js
+++ components/storage/test/unit/database.test.js
@@ -3,6 +3,8 @@
 /* global describe, it, beforeEach */
 
 const chai = require('chai');
+const assert = chai.assert;
+const bluebird = require('bluebird');
 
 const Database = require('../../src/Database');
 
@@ -25,5 +27,40 @@ describe('Database', () => {
       await database.close();
     });
   });
-});
 
+  describe('isDuplicateError utility', () => {
+
+    const collectionInfo = {
+      name: 'duplicateTest',
+      indexes: [{
+        index: {duplicateKey: 1},
+        options: {unique: true}
+      }]
+    };
+
+    beforeEach((done) => {
+      database.dropCollection(collectionInfo, (err) => {
+        done(err);
+      });
+    });
+
+    // cf. GH issue #163
+    it('must detect mongo duplicate errors correctly', async () => {
+
+      const duplicateMsg = `E11000 duplicate key error collection: ${connectionSettings.name}.${collectionInfo.name} index:`;
+
+      // First insert, should succeed
+      database.insertOne(collectionInfo, {duplicateKey: 'duplicate'}, (err, res) => {
+        assert.exists(res);
+        assert.notExists(err);
+        // Second insert, should lead to duplicate error
+        database.insertOne(collectionInfo, {duplicateKey: 'duplicate'}, (err, res) => {
+          assert.exists(err);
+          assert.isTrue(Database.isDuplicateError(err));
+          assert.include(err.errmsg, duplicateMsg, 'Mongo duplicate error message changed!');
+          assert.notExists(res);
+        });
+      }); 
+    });
+  });
+});
