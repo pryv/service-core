41b261976a814bdd0e14f89747d6fe9a79aad77c
Ilia Kebets <kebetsi@gmail.com>
2019-05-09 11:36:34 +0200

    fix remaining folders linking to old mongoDB folder

--- scripts/start-database.sh
+++ scripts/start-database.sh
@@ -5,8 +5,5 @@ SCRIPT_FOLDER=$(cd $(dirname "$0"); pwd)
 cd $SCRIPT_FOLDER/..
 
 
-if [ `uname` = "Linux" ]; then
-    ../mongodb-linux-x86_64-3.4.20/bin/mongod --smallfiles --dbpath ../mongodb-data
-else
-    ../mongodb/bin/mongod --smallfiles --dbpath ../mongodb-data
-fi
+../mongo-bin/bin/mongod --smallfiles --dbpath ../mongodb-data
+
