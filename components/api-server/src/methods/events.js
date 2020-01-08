5177fc179ef63e7da952302cf3ae6b3b09a9f697
loic <loic@pryv.com>
2020-01-08 11:45:16 +0100

    Forbid to change type of HF-event

--- components/api-server/src/methods/events.js
+++ components/api-server/src/methods/events.js
@@ -397,6 +397,22 @@ module.exports = function (
         return next(errors.forbidden());
       }
 
+      const currentEventType = event.type;
+      const isCurrentEventTypeSeries = isSeriesType(currentEventType);
+      if (! typeRepo.isKnown(currentEventType) && isCurrentEventTypeSeries) {
+        return next(errors.invalidEventType(currentEventType)); // We forbid the 'series' prefix for these free types. 
+      }
+
+      const updatedEventType = params.update.type;
+      const isUpdatedEventTypeSeries = isSeriesType(updatedEventType);
+      if (! typeRepo.isKnown(updatedEventType) && isUpdatedEventTypeSeries) {
+        return next(errors.invalidEventType(updatedEventType)); // We forbid the 'series' prefix for these free types. 
+      }
+
+      if(isCurrentEventTypeSeries != isUpdatedEventTypeSeries && (isCurrentEventTypeSeries || isUpdatedEventTypeSeries)) {
+        return next(errors.invalidOperation('Normal events can\'t be updated to HF-events and HF-events can\'t be updated to normal event'));
+      }
+
       context.oldContent = _.cloneDeep(event);
       context.content = _.extend(event, params.update);
 
