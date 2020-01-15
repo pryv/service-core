dfdfc88fef7b6ed83a624b8c9ee3496843cd4950
loic <loic@pryv.com>
2020-01-15 17:08:03 +0100

    cleaner (and less glitched...) code

--- components/middleware/src/getAuth.js
+++ components/middleware/src/getAuth.js
@@ -5,27 +5,26 @@ const ApiConstants = require('components/hfs-server/src/web/api_constants');
 // Middleware that verifies the presence of an authorization token
 // 
 module.exports = (req: express$Request, res: express$Response, next: express$NextFunction) => {
-  let authHeader = req.headers[ApiConstants.AUTH_HEADER];
-  const authQuery = req.query.auth;
-
-  if ((authHeader == null || authHeader === '') && (authQuery == null || authQuery === '')) {
-    // return next(new Error('Missing \'Authorization\' header or \'auth\' query parameter.'));
-    return next();
-  }
-
-  // Basic auth support
-  // See service-core/components/middleware/src/initContext.js for reference
-  if (authHeader != null) {
-    const authBasic = authHeader.split(' ');
-    if (authBasic[0].toLowerCase() === 'basic' && authBasic[1] != null) {
-      // Note: since our Basic scheme do not contain the username, the token is in first position
-      // example: https://token@username.pryv.me/
-      authHeader = Buffer.from(authBasic[1], 'base64').toString('ascii').split(':')[0];
-    }
-  }
-
-  // Set authorization token in the context
-  const auth = authHeader || authQuery;
-  req.headers[ApiConstants.AUTH_HEADER] = auth; // It's where hfs-server look for authorization token
+  req.headers[ApiConstants.AUTH_HEADER] = getAuth(req);
   next();
 };
+
+function getAuth(req): ?string {
+  let authorizationHeader = req.header('authorization');
+
+  if (authorizationHeader != null) {
+    const basic = authorizationHeader.split(' ');
+    if (basic[0].toLowerCase() === 'basic' && basic[1]) {
+      authorizationHeader = Buffer.from(basic[1], 'base64').toString('ascii').split(':')[0];
+    }
+    if (Array.isArray(authorizationHeader)) return authorizationHeader[0];
+    return authorizationHeader;        
+  }
+
+  // assert: no authorization in header, let's check query: 
+  const authFromQuery = req.query.auth; 
+
+  if (authFromQuery == null) return null; 
+  if (Array.isArray(authFromQuery)) return authFromQuery[0];
+  return authFromQuery;
+}
