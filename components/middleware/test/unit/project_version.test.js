49083a0274ed12fa4a876ee5609e03d95351801f
Ilia Kebets <kebetsi@gmail.com>
2020-06-05 13:30:18 +0200

    remove useless tests from middleware and set .api-version file used in tests only to 1.2.3 to match expected value in said tests

--- components/middleware/test/unit/project_version.test.js
+++ components/middleware/test/unit/project_version.test.js
@@ -21,32 +21,9 @@ describe('ProjectVersion#version', () => {
   
   describe('when a ".api-version" file exists in the project', () => {
     const versionFilePath = path.join(__dirname, '../../../../../', '.api-version');
-
-    beforeEach(() => {
-      fs.writeFileSync(versionFilePath, '1.2.3');      
-    });
-    afterEach(() => {
-      fs.unlinkSync(versionFilePath);
-    });
     
     it('[HV40] reads .api-version and returns that constant', async () => {
       assert.strictEqual(await pv.version(), '1.2.3');
-    });
-  });
-  describe('when neither method works', () => {
-    it('[KTG9] throws an error', async () => {
-      const failsAlways = sinon.stub(pv, 'exec');
-      failsAlways.throws(); 
-      
-      let thrown = false; 
-      try {
-        await pv.version();
-      }
-      catch(err) {
-        thrown = true;
-      }
-
-      assert.isTrue(thrown);
     });
   });
 });
