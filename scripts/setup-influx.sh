25c16839c73f2cd4dc7c7ded9caa9007759ea425
Ieva <ieva.navikiene@gmail.com>
2020-06-22 11:56:27 +0200

    tsting worflow

--- scripts/setup-influx.sh
+++ scripts/setup-influx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-./etc/lsb-release
+#curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+#./etc/lsb-release
 #echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-sudo apt-get update && apt-get install influxdb
+#echo "deb https://repos.influxdata.com/ubuntu bionic stable" | tee /etc/apt/sources.list.d/influxdb.list
+#apt-get update && apt-get install influxdb
