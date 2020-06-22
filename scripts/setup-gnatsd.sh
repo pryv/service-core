4a6f5cbe64752d02aec25e651ef897ddd9b834d6
Ieva <ieva.navikiene@gmail.com>
2020-06-22 10:30:01 +0200

    changed installation scripts

--- scripts/setup-gnatsd.sh
+++ scripts/setup-gnatsd.sh
@@ -7,24 +7,26 @@
 # command -v gnatsd should output /usr/local/bin/gnatsd
 program="${gnatsd}"
 command -v "${program}"
-if [[ "${?}" -ne 0 ]]; then
-    VERSION="v1.0.4"
-    BASENAME="gnatsd-$VERSION-linux-amd64"
-    FILENAME="$BASENAME.zip"
-    SOURCE_URL="https://github.com/nats-io/gnatsd/releases/download/$VERSION/$FILENAME"
-    SPOOLDIR=/var/spool
+#if [[ "${?}" -ne 0 ]]; then
 
-    apt-get install -y unzip
+VERSION="v1.0.4"
+BASENAME="gnatsd-$VERSION-linux-amd64"
+FILENAME="$BASENAME.zip"
+SOURCE_URL="https://github.com/nats-io/gnatsd/releases/download/$VERSION/$FILENAME"
+SPOOLDIR=/var/spool
 
-    mkdir -p $SPOOLDIR/gnatsd && \
-          cd $SPOOLDIR/gnatsd/ && 
-          curl -L -O $SOURCE_URL && 
-          unzip $FILENAME
+apt-get install -y unzip
 
-    echo "24446c1be57d08ccc386a240a8ab5b78668e4db5d0c7878d548d3f95b90cb76b  $FILENAME" | \
-         sha256sum -c -
+mkdir -p $SPOOLDIR/gnatsd && \
+	  cd $SPOOLDIR/gnatsd/ && 
+	  curl -L -O $SOURCE_URL && 
+	  unzip $FILENAME
 
-    mv $BASENAME/gnatsd /usr/local/bin/
-else
-    echo "...skipped: Gnatsd already installed"
-fi
+echo "24446c1be57d08ccc386a240a8ab5b78668e4db5d0c7878d548d3f95b90cb76b  $FILENAME" | \
+	 sha256sum -c -
+
+mv $BASENAME/gnatsd /usr/local/bin/
+
+#else
+#    echo "...skipped: Gnatsd already installed"
+#fi
--- scripts/setup-influx.sh
+++ scripts/setup-influx.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-source /etc/lsb-release
-echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-apt-get update && apt-get install influxdb
+./etc/lsb-release
+#echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt-get update && apt-get install influxdb
