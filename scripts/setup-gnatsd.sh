5bdeee8f440892e69f5c2f6a03daae11ce091419
Ieva <ieva.navikiene@gmail.com>
2020-06-22 10:52:49 +0200

    testing workflow

--- scripts/setup-gnatsd.sh
+++ scripts/setup-gnatsd.sh
@@ -13,19 +13,19 @@ VERSION="v1.0.4"
 BASENAME="gnatsd-$VERSION-linux-amd64"
 FILENAME="$BASENAME.zip"
 SOURCE_URL="https://github.com/nats-io/gnatsd/releases/download/$VERSION/$FILENAME"
-SPOOLDIR=/var/spool
+#SPOOLDIR=/var/spool
 
 apt-get install -y unzip
 
-mkdir -p $SPOOLDIR/gnatsd && \
-	  cd $SPOOLDIR/gnatsd/ && 
+mkdir -p ./gnatsd && \
+	  cd ./gnatsd/ && 
 	  curl -L -O $SOURCE_URL && 
 	  unzip $FILENAME
 
 echo "24446c1be57d08ccc386a240a8ab5b78668e4db5d0c7878d548d3f95b90cb76b  $FILENAME" | \
 	 sha256sum -c -
 
-mv $BASENAME/gnatsd /usr/local/bin/
+#mv $BASENAME/gnatsd /usr/local/bin/
 
 #else
 #    echo "...skipped: Gnatsd already installed"
