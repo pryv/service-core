6814278529e7ac04165338fb22950fd0fa079c32
Ieva <ieva.navikiene@gmail.com>
2020-06-23 15:51:26 +0200

    renaming the workflow and fixing few grammar errors

--- scripts/setup-gnatsd.sh
+++ scripts/setup-gnatsd.sh
@@ -5,15 +5,10 @@
 
 # check if gnatsd is already installed and if no install it
 # command -v gnatsd should output /usr/local/bin/gnatsd
-program="${gnatsd}"
-command -v "${program}"
-#if [[ "${?}" -ne 0 ]]; then
-
 VERSION="v1.0.4"
 BASENAME="gnatsd-$VERSION-linux-amd64"
 FILENAME="$BASENAME.zip"
 SOURCE_URL="https://github.com/nats-io/gnatsd/releases/download/$VERSION/$FILENAME"
-#SPOOLDIR=/var/spool
 
 apt-get install -y unzip
 
@@ -22,11 +17,4 @@ mkdir -p ./gnatsd && \
 	  curl -L -O $SOURCE_URL && 
 	  unzip $FILENAME
 
-echo "24446c1be57d08ccc386a240a8ab5b78668e4db5d0c7878d548d3f95b90cb76b  $FILENAME" | \
-	 sha256sum -c -
-
-#mv $BASENAME/gnatsd /usr/local/bin/
-
-#else
-#    echo "...skipped: Gnatsd already installed"
-#fi
+echo "24446c1be57d08ccc386a240a8ab5b78668e4db5d0c7878d548d3f95b90cb76b  $FILENAME" | sha256sum -c -
