8bc9ae6bf9ab60d904695a14e06e460e9c16a2b5
Perki <pm@perki.com>
2020-12-03 14:21:58 +0100

    changing create-only fetch return when unauthorized

--- components/api-server/test/permissions-create-only.test.js
+++ components/api-server/test/permissions-create-only.test.js
@@ -316,7 +316,7 @@ describe('permissions create-only level', () => {
     }
 
     describe('GET /', function() {
-      it('[CKF3] should return an empty list when fetching "create-only" streams', async function() {
+      it('[CKF3] should return an error list when fetching explicitly "create-only" streams', async function() {
         const query = {
           streams: [createOnlyStreamId]
         };
@@ -326,8 +326,9 @@ describe('permissions create-only level', () => {
           .get(basePath)
           .set('Authorization', createOnlyToken)
           .query(query);
-        assert.equal(res.status, 200);
-        assert.equal(res.body.events.length, 0);
+
+        assert.equal(res.status, 400);
+        assert.equal(res.body.error.id, 'unknown-referenced-resource');
       });
 
       it('[V4KJ] should return events when fetching "create-only" streams that are children of "read" streams', async function() {
@@ -449,7 +450,7 @@ describe('permissions create-only level', () => {
         fileId = res.body.event.fileId;
       });
       
-      // cleaning up explicitely as we are not using fixtures
+      // cleaning up explicitly as we are not using fixtures
       after(async function () {
         await server.request()
           .delete(reqPath(eventId))
