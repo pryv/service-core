f886022829f2ef55c29e5d425a113cb17641b373
Ilia Kebets <kebetsi@gmail.com>
2021-01-20 11:05:09 +0100

    fix undefined storageLayer

--- components/api-server/src/application.js
+++ components/api-server/src/application.js
@@ -31,7 +31,7 @@ const expressAppInit = require('./expressApp');
 const middleware = require('components/middleware');
 const errorsMiddlewareMod = require('./middleware/errors'); 
 
-const { getConfig, getLogger } = require('boiler');
+const { getConfig, getLogger, getConfigUnsafe } = require('boiler');
 const logger = getLogger('application');
 
 const { Extension, ExtensionLoader } = require('components/utils').extension;
@@ -75,6 +75,7 @@ class Application {
     this.api = new API(); 
     this.systemAPI = new API(); 
   
+    this.produceStorageSubsystem(); 
     logger.debug('created');
   }
 
@@ -87,8 +88,7 @@ class Application {
     logger.debug('Init started');
 
     this.config = await getConfig();
-   
-    this.produceStorageSubsystem(); 
+
     await this.createExpressApp();
     this.initiateRoutes();
     this.expressApp.use(middleware.notFound);
@@ -138,7 +138,7 @@ class Application {
   }
 
   produceStorageSubsystem() {
-    const config = this.config;
+    const config = getConfigUnsafe();
     this.database = new storage.Database(config.get('database'));
 
     // 'StorageLayer' is a component that contains all the vertical registries
