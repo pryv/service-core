876bee80f6e4c897d55426e693ef8f6bbd2946f8
Ilia Kebets <kebetsi@gmail.com>
2021-01-20 15:21:34 +0100

    add missing license

--- components/api-server/config/config-validation.js
+++ components/api-server/config/config-validation.js
@@ -1,4 +1,10 @@
 /**
+ * @license
+ * Copyright (C) 2020 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+/**
  * Plugin to run at the end of the config loading.
  * Should validate (or not) the configuration and display appropriate messages
  */
