e4080618df49057d2dbb37495936919afccf1aab
Ilia Kebets <kebetsi@gmail.com>
2021-01-27 10:29:33 +0100

    Revert "load some methods conditionnally"
    This reverts commit b1c9e305a8bc25279c1f7460f3ab2a70690e3ba3.

--- components/api-server/src/server.js
+++ components/api-server/src/server.js
@@ -108,18 +108,16 @@ class Server {
       application.storageLayer.sessions, 
       application.storageLayer.events, 
       config.get('auth'));
-      
-    if (this.isDnsLess) {
-      require('./methods/auth/register-dnsless')(application.api, 
-        application.logging, 
-        application.storageLayer, 
-        config.get('services'));
-    } else {
-      require('./methods/auth/register')(application.api, 
-        application.logging, 
-        application.storageLayer, 
-        config.get('services'));
-    }
+    
+    require('./methods/auth/register')(application.api, 
+      application.logging, 
+      application.storageLayer, 
+      config.get('services'));
+
+    require('./methods/auth/register-dnsless')(application.api, 
+      application.logging, 
+      application.storageLayer, 
+      config.get('services'));
 
     if (this.isOpenSource) {
       require('./methods/auth/delete-opensource')(application.api,
