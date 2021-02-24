c49812111f1e7dc7e0f7233dc0597baf79251c06
Ilia Kebets <kebetsi@gmail.com>
2021-02-24 18:29:39 +0100

    add test to verify audit log storage - nothing for now

--- components/audit/test/acceptance/Audit.test.js
+++ components/audit/test/acceptance/Audit.test.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2012-2021 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 
 
 /* global describe, before, after, it, assert, cuid, audit, config, initTests, closeTests, initCore, coreRequest, mongoFixtures */
@@ -6,23 +12,24 @@ describe('Audit', function() {
   let userid = cuid();
   let createdBy = cuid();
 
-  let username, access, basePath;
+  let user, username, access, basePath;
+  let auditStorage;
 
   before(async function() {
     await initTests();
     await initCore();
+    user = await mongoFixtures.user(charlatan.Lorem.characters(7), {});
 
-    const user = await mongoFixtures.user(charlatan.Lorem.characters(7), {
-      
-    });
     username = user.attrs.username;
-    basePath = '/' + user.attrs.username + '/events';
+    basePath = '/' + username + '/events';
     access = await user.access({
       type: 'personal',
       token: cuid(),
     });
     access = access.attrs;
     await user.session(access.token);
+    user = user.attrs;
+    console.log('created', user);
   });
 
   after(async function() {
@@ -36,11 +43,17 @@ describe('Audit', function() {
       res = await coreRequest
         .get(basePath)
         .set('Authorization', access.token);
-      console.log('got', res.body)
+      //console.log('got', res.body)
     });
 
     it('must return 200', function () {
       assert.equal(res.status, 200);
+    });
+    it('must log it into the database', function () {
+      auditStorage = audit.storage.forUser(user.id);
+      const entries = auditStorage.getLogs({createdBy: createdBy});
+      console.log('got', entries);
+      assert.exists(entries);
     });
     
   });
