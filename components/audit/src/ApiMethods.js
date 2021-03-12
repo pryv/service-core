3f33e9c77d5284cc008ed7bb624fde0b2cbe41a4
Ilia Kebets <kebetsi@gmail.com>
2021-03-12 14:42:14 +0100

    whether methods are audited or not is handled by ApiMethods.js, methods with no user get a no-user userId

--- components/audit/src/ApiMethods.js
+++ components/audit/src/ApiMethods.js
@@ -61,25 +61,32 @@ const NOT_AUDITED_METHODS = [
   'service.info',
   'system.createPoolUser',
   'system.getUsersPoolSize',
+  'system.getUserInfo',
   'auth.usernameCheck',
+  'auth.emailCheck',
 ];
 
 const AUDITED_METHODS = ALL_METHODS.filter(m => {
   return ! NOT_AUDITED_METHODS.includes(m);
 });
 
+// doesnt include non-audited ones
+const WITHOUT_USER_METHODS = [
+  'auth.register',
+  'system.createUser',
+];
+
 const auditedMethodsMap = {};
-AUDITED_METHODS.forEach(m => {
-  auditedMethodsMap[m] = true;
-});
-const ALL_METHODS_MAP = {};
-ALL_METHODS.forEach(m => {
-  ALL_METHODS_MAP[m] = true;
-});
+AUDITED_METHODS.forEach(m => auditedMethodsMap[m] = true);
+const allMethodsMap = {};
+ALL_METHODS.forEach(m => allMethodsMap[m] = true);
+const withoutUserMethodsMap = {};
+WITHOUT_USER_METHODS.forEach(m => withoutUserMethodsMap[m] = true);
+
 
 function isMethodDeclared(methodId) {
   if (methodId.includes('*')) return true; // allowing to register for wildcards such as "followedSlices.*", or "*"
-  if (ALL_METHODS_MAP[methodId]) return true;
+  if (allMethodsMap[methodId]) return true;
   return false;
 }
 
@@ -87,6 +94,8 @@ module.exports = {
   AUDITED_METHODS_MAP: auditedMethodsMap,
   AUDITED_METHODS: AUDITED_METHODS,
   ALL_METHODS: ALL_METHODS,
-  ALL_METHODS_MAP: ALL_METHODS_MAP,
+  ALL_METHODS_MAP: allMethodsMap,
+  WITHOUT_USER_METHODS: WITHOUT_USER_METHODS,
+  WITHOUT_USER_METHODS_MAP: withoutUserMethodsMap,
   isMethodDeclared: isMethodDeclared,
 };
