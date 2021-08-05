f7d6091cb96c616cceb0c8b9e4a1aeaf7bee621b
Perki <pm@perki.com>
2021-08-05 12:07:23 +0200

    Adding License

--- components/cache/test/unit/synchro.test.js
+++ components/cache/test/unit/synchro.test.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2012-2021 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 const cache = require('cache');
 const synchro = require('../../src/synchro');
 const { pubsub } = require('messages');
