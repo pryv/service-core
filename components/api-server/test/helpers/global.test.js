1abd3d312669f85d7396e92590ceb8ded831b69e
Perki <perki@pryv.com>
2021-09-15 17:40:04 +0200

    licensing

--- components/api-server/test/helpers/global.test.js
+++ components/api-server/test/helpers/global.test.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (C) 2012-2021 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
 const integrityFinalCheck = require('test-helpers/src/integrity-final-check');
 
 afterEach(async function () {
