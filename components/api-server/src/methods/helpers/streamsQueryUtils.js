8d3bb07c4fcadd004212fc21f50c1a2bdb7287f1
Perki <perki@pryv.com>
2022-02-18 11:09:51 +0100

    Finalizing Fix by removing ghost "*" produced by Mall

--- components/api-server/src/methods/helpers/streamsQueryUtils.js
+++ components/api-server/src/methods/helpers/streamsQueryUtils.js
@@ -171,7 +171,7 @@ function uniqueStreamIds(arrayOfStreamiIs) {
  * @returns
  */
 exports.expandAndTransformStreamQueries = async function expandAndTransformStreamQueries(streamQueries, expandStream) {
-
+  
   async function expandSet(streamIds, storeId, excludedIds = []) {
     const expandedSet = new Set(); // use a Set to avoid duplicate entries;
     for (let streamId of streamIds) {
