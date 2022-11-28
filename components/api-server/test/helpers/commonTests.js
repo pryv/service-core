c97238e799337c4b971e61852f7ee4e90bc88ce3
Simon Goumaz <simon@attentif.ch>
2022-11-28 17:05:38 +0100

    More, more linting errors fixed

--- components/api-server/test/helpers/commonTests.js
+++ components/api-server/test/helpers/commonTests.js
@@ -8,16 +8,18 @@
  * Regroups acceptance tests reused in different places.
  */
 
+const { assert } = require('chai');
+const request = require('superagent');
+
 const validation = require('./validation');
 const ErrorIds = require('errors').ErrorIds;
-const request = require('superagent');
 
 /**
  * @param {String} serverURL
  * @param {String} path
  */
 exports.checkAccessTokenAuthentication = function (serverURL, path, done) {
-  request.get(new URL(path, serverURL).toString()).end(function (err, res) {
+  request.get(new URL(path, serverURL).toString()).end(function (err, res) { // eslint-disable-line n/handle-callback-err
     validation.check(res, {
       status: 401,
       id: ErrorIds.InvalidAccessToken
