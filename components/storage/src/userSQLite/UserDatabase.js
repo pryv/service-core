0e09695a5bf8597478b0868216f20cf3bc3f04c0
Simon Goumaz <simon@attentif.ch>
2023-06-19 13:01:25 +0200

    Preliminary refactor (use prototype style for clearer structure)

--- components/storage/src/userSQLite/UserDatabase.js
+++ components/storage/src/userSQLite/UserDatabase.js
@@ -13,248 +13,229 @@ const eventsSchema = require('./schema/events');
 const { createFTSFor } = require('./FullTextSearchDataBase');
 const { toSQLiteQuery } = require('./sqLiteStreamQueryUtils');
 
+module.exports = UserDatabase;
+
 const DB_OPTIONS = {};
 
 const tables = {
   events: eventsSchema.dbSchema
 };
 
-const ALL_EVENTS_TAG = eventsSchema.ALL_EVENTS_TAG;
+/**
+ * @param {Object} params
+ * @param {string} params.dbPath Path to the SQLite database file
+ */
+function UserDatabase (logger, params) {
+  this.logger = logger.getLogger('user-database');
+  this.db = new SQLite3(params.dbPath, DB_OPTIONS);
+}
+
+UserDatabase.prototype.init = async function () {
+  await concurrentSafeWrite.initWALAndConcurrentSafeWriteCapabilities(this.db);
+  // here we might want to skip DB initialization if version is not null
+
+  this.create = {};
+  this.getAll = {};
+  this.get = {};
+  this.delete = {};
+
+  // create all tables
+  for (const tableName of Object.keys(tables)) {
+    const columnsTypes = [];
+    const indexes = [];
+    const columnNames = Object.keys(tables[tableName]);
+    columnNames.forEach((columnName) => {
+      const column = tables[tableName][columnName];
+      columnsTypes.push(`${columnName} ${column.type}`);
+      if (column.index) { indexes.push(columnName); }
+    });
+
+    await concurrentSafeWrite.execute(() => {
+      this.db.prepare('CREATE TABLE IF NOT EXISTS events ( ' +
+        columnsTypes.join(', ') +
+      ');').run();
+    });
+
+    for (const columnName of indexes) {
+      await concurrentSafeWrite.execute(() => {
+        this.db.prepare(`CREATE INDEX IF NOT EXISTS ${tableName}_${columnName} ON ${tableName}(${columnName})`).run();
+      });
+    }
+
+    this.create[tableName] = this.db.prepare(`INSERT INTO ${tableName} (` +
+      columnNames.join(', ') + ') VALUES (@' +
+      columnNames.join(', @') + ')');
+
+    this.getAll[tableName] = this.db.prepare(`SELECT * FROM ${tableName}`);
+  }
+
+  // -- create FTS for streamIds on events
+  createFTSFor(this.db, 'events', tables.events, ['streamIds']);
+
+  this.queryGetTerms = this.db.prepare('SELECT * FROM events_fts_v WHERE term like ?');
+
+  this.delete.eventsByHeadId = this.db.prepare('DELETE FROM events WHERE headId = ?');
+  this.delete.eventById = this.db.prepare('DELETE FROM events WHERE eventid = ?');
+
+  this.get.eventById = this.db.prepare('SELECT * FROM events WHERE eventid = ?');
+  this.get.eventsDeletedSince = this.db.prepare('SELECT * from events WHERE deleted >= ? ORDER BY deleted DESC');
+  this.get.eventHistory = this.db.prepare('SELECT * from events WHERE headId = ? ORDER BY modified ASC');
+};
+
+UserDatabase.prototype.updateEvent = async function (eventId, eventData) {
+  const eventForDb = eventsSchema.toDB(eventData);
+  if (eventForDb.streamIds == null) { eventForDb.streamIds = eventsSchema.ALL_EVENTS_TAG; }
+
+  delete eventForDb.eventid;
+  const queryString = `UPDATE events SET ${Object.keys(eventForDb).map(field => `${field} = @${field}`).join(', ')} WHERE eventid = @eventid`;
+  eventForDb.eventid = eventId;
+  const update = this.db.prepare(queryString);
+
+  await concurrentSafeWrite.execute(() => {
+    const res = update.run(eventForDb);
+    this.logger.debug(`UPDATE events changes: ${res.changes} eventId: ${eventId} event: ${JSON.stringify(eventForDb)}`);
+    if (res.changes !== 1) {
+      throw new Error('Event not found');
+    }
+  });
+
+  const resultEvent = eventsSchema.fromDB(eventForDb);
+  return resultEvent;
+};
 
 /**
- * TODO: refactor the structure of tables and queries
- *       (currently not consistent, either internally or with the Mongo code)
+ * Use only during tests or migration
+ * Not safe within a multi-process environement
  */
-class UserDatabase {
-  /**
-   * SQLite3 instance
-   */
-  db;
+UserDatabase.prototype.createEventSync = function (event) {
+  const eventForDb = eventsSchema.toDB(event);
+  this.logger.debug(`(sync) CREATE event: ${JSON.stringify(eventForDb)}`);
+  this.create.events.run(eventForDb);
+};
 
-  create;
-  get;
-  getAll;
-
-  queryGetTerms;
-
-  logger;
-  version;
-
-  /**
-   *
-   * @param {Object} params
-   * @param {string} params.dbPath // the file to use as database
-   */
-  constructor (logger, params) {
-    this.logger = logger.getLogger('user-database');
-    this.db = new SQLite3(params.dbPath, DB_OPTIONS);
-  }
-
-  async init () {
-    await concurrentSafeWrite.initWALAndConcurrentSafeWriteCapabilities(this.db);
-    // here we might want to skip DB initialization if version is not null
-
-    this.create = {};
-    this.getAll = {};
-    this.get = {};
-    this.delete = {};
-
-    // create all tables
-    for (const tableName of Object.keys(tables)) {
-      const columnsTypes = [];
-      const indexes = [];
-      const columnNames = Object.keys(tables[tableName]);
-      columnNames.forEach((columnName) => {
-        const column = tables[tableName][columnName];
-        columnsTypes.push(`${columnName} ${column.type}`);
-        if (column.index) { indexes.push(columnName); }
-      });
-
-      await concurrentSafeWrite.execute(() => {
-        this.db.prepare('CREATE TABLE IF NOT EXISTS events ( ' +
-          columnsTypes.join(', ') +
-        ');').run();
-      });
-
-      for (const columnName of indexes) {
-        await concurrentSafeWrite.execute(() => {
-          this.db.prepare(`CREATE INDEX IF NOT EXISTS ${tableName}_${columnName} ON ${tableName}(${columnName})`).run();
-        });
-      }
-
-      this.create[tableName] = this.db.prepare(`INSERT INTO ${tableName} (` +
-        columnNames.join(', ') + ') VALUES (@' +
-        columnNames.join(', @') + ')');
-
-      this.getAll[tableName] = this.db.prepare(`SELECT * FROM ${tableName}`);
-    }
-
-    // -- create FTS for streamIds on events
-    createFTSFor(this.db, 'events', tables.events, ['streamIds']);
-
-    this.queryGetTerms = this.db.prepare('SELECT * FROM events_fts_v WHERE term like ?');
-
-    this.delete.eventsByHeadId = this.db.prepare('DELETE FROM events WHERE headId = ?');
-    this.delete.eventById = this.db.prepare('DELETE FROM events WHERE eventid = ?');
-
-    this.get.eventById = this.db.prepare('SELECT * FROM events WHERE eventid = ?');
-    this.get.eventsDeletedSince = this.db.prepare('SELECT * from events WHERE deleted >= ? ORDER BY deleted DESC');
-    this.get.eventHistory = this.db.prepare('SELECT * from events WHERE headId = ? ORDER BY modified ASC');
-  }
-
-  async updateEvent (eventId, eventData) {
-    const eventForDb = eventsSchema.toDB(eventData);
-    if (eventForDb.streamIds == null) { eventForDb.streamIds = ALL_EVENTS_TAG; }
-
-    delete eventForDb.eventid;
-    const queryString = `UPDATE events SET ${Object.keys(eventForDb).map(field => `${field} = @${field}`).join(', ')} WHERE eventid = @eventid`;
-    eventForDb.eventid = eventId;
-    const update = this.db.prepare(queryString);
-
-    await concurrentSafeWrite.execute(() => {
-      const res = update.run(eventForDb);
-      this.logger.debug(`UPDATE events changes: ${res.changes} eventId: ${eventId} event: ${JSON.stringify(eventForDb)}`);
-      if (res.changes !== 1) {
-        throw new Error('Event not found');
-      }
-    });
-
-    const resultEvent = eventsSchema.fromDB(eventForDb);
-    return resultEvent;
-  }
-
-  /**
-   * Use only during tests or migration
-   * Not safe within a multi-process environement
-   */
-  createEventSync (event) {
-    const eventForDb = eventsSchema.toDB(event);
-    this.logger.debug(`(sync) CREATE event: ${JSON.stringify(eventForDb)}`);
+UserDatabase.prototype.createEvent = async function (event) {
+  const eventForDb = eventsSchema.toDB(event);
+  this.logger.debug(`(async) CREATE event: ${JSON.stringify(eventForDb)}`);
+  await concurrentSafeWrite.execute(() => {
     this.create.events.run(eventForDb);
-  }
+  });
+};
 
-  async createEvent (event) {
-    const eventForDb = eventsSchema.toDB(event);
-    this.logger.debug(`(async) CREATE event: ${JSON.stringify(eventForDb)}`);
-    await concurrentSafeWrite.execute(() => {
-      this.create.events.run(eventForDb);
-    });
-  }
+UserDatabase.prototype.getAllActions = function () {
+  return this.queryGetTerms.all('action-%');
+};
 
-  getAllActions () {
-    return this.queryGetTerms.all('action-%');
-  }
+UserDatabase.prototype.getAllAccesses = function () {
+  return this.queryGetTerms.all('access-%');
+};
 
-  getAllAccesses () {
-    return this.queryGetTerms.all('access-%');
-  }
+UserDatabase.prototype.deleteEventsHistory = async function (eventId) {
+  this.logger.debug(`(async) DELETE event history for eventId: ${eventId}`);
+  await concurrentSafeWrite.execute(() => {
+    return this.delete.eventsByHeadId.run(eventId);
+  });
+};
 
-  async deleteEventsHistory (eventId) {
-    this.logger.debug(`(async) DELETE event history for eventId: ${eventId}`);
-    await concurrentSafeWrite.execute(() => {
-      return this.delete.eventsByHeadId.run(eventId);
-    });
-  }
+UserDatabase.prototype.minimizeEventHistory = async function (eventId, fieldsToRemove) {
+  const minimizeHistoryStatement = `UPDATE events SET ${fieldsToRemove.map(field => `${field} = ${field === 'streamIds' ? '\'' + eventsSchema.ALL_EVENTS_TAG + '\'' : 'NULL'}`).join(', ')} WHERE headId = ?`;
+  this.logger.debug(`(async) Minimize event history: ${minimizeHistoryStatement}`);
+  await concurrentSafeWrite.execute(() => {
+    this.db.prepare(minimizeHistoryStatement).run(eventId);
+  });
+};
 
-  async minimizeEventHistory (eventId, fieldsToRemove) {
-    const minimizeHistoryStatement = `UPDATE events SET ${fieldsToRemove.map(field => `${field} = ${field === 'streamIds' ? '\'' + ALL_EVENTS_TAG + '\'' : 'NULL'}`).join(', ')} WHERE headId = ?`;
-    this.logger.debug(`(async) Minimize event history: ${minimizeHistoryStatement}`);
-    await concurrentSafeWrite.execute(() => {
-      this.db.prepare(minimizeHistoryStatement).run(eventId);
-    });
-  }
+UserDatabase.prototype.deleteEvents = async function (params) {
+  const queryString = prepareEventsDeleteQuery(params);
+  if (queryString.indexOf('MATCH') > 0) {
+    this.logger.debug(`DELETE events one by one as queryString includes MATCH: ${queryString}`);
+    // SQLite does not know how to delete with "MATCH" statement
+    // going by the doddgy task of getting events that matches the query and deleting them one by one
+    const selectEventsToBeDeleted = prepareEventsGetQuery(params);
 
-  async deleteEvents (params) {
-    const queryString = prepareEventsDeleteQuery(params);
-    if (queryString.indexOf('MATCH') > 0) {
-      this.logger.debug(`DELETE events one by one as queryString includes MATCH: ${queryString}`);
-      // SQLite does not know how to delete with "MATCH" statement
-      // going by the doddgy task of getting events that matches the query and deleting them one by one
-      const selectEventsToBeDeleted = prepareEventsGetQuery(params);
-
-      for (const event of this.db.prepare(selectEventsToBeDeleted).iterate()) {
-        this.logger.debug(`  > DELETE event: ${event.eventid}`);
-        await concurrentSafeWrite.execute(() => {
-          this.delete.eventById.run(event.eventid);
-        });
-      }
-      return null;
-    }
-    // else
-    let res = null;
-    this.logger.debug(`DELETE events: ${queryString}`);
-    await concurrentSafeWrite.execute(() => {
-      res = this.db.prepare(queryString).run();
-    });
-    return res;
-  }
-
-  getOneEvent (eventId) {
-    this.logger.debug(`GET ONE event: ${eventId}`);
-    const event = this.get.eventById.get(eventId);
-    if (event == null) return null;
-    return eventsSchema.fromDB(event);
-  }
-
-  getEvents (params) {
-    params.query.push({ type: 'equal', content: { field: 'deleted', value: null } });
-    params.query.push({ type: 'equal', content: { field: 'headId', value: null } });
-    const queryString = prepareEventsGetQuery(params);
-    this.logger.debug(`GET Events: ${queryString}`);
-    const res = this.db.prepare(queryString).all();
-    if (res != null) {
-      return res.map(eventsSchema.fromDB);
+    for (const event of this.db.prepare(selectEventsToBeDeleted).iterate()) {
+      this.logger.debug(`  > DELETE event: ${event.eventid}`);
+      await concurrentSafeWrite.execute(() => {
+        this.delete.eventById.run(event.eventid);
+      });
     }
     return null;
   }
+  // else
+  let res = null;
+  this.logger.debug(`DELETE events: ${queryString}`);
+  await concurrentSafeWrite.execute(() => {
+    res = this.db.prepare(queryString).run();
+  });
+  return res;
+};
 
-  getEventsStream (params) {
-    params.query.push({ type: 'equal', content: { field: 'deleted', value: null } });
-    params.query.push({ type: 'equal', content: { field: 'headId', value: null } });
-    const queryString = prepareEventsGetQuery(params);
-    this.logger.debug(`GET Events Stream: ${queryString}`);
-    const query = this.db.prepare(queryString);
-    return this.readableEventsStreamForIterator(query.iterate());
+UserDatabase.prototype.getOneEvent = function (eventId) {
+  this.logger.debug(`GET ONE event: ${eventId}`);
+  const event = this.get.eventById.get(eventId);
+  if (event == null) return null;
+  return eventsSchema.fromDB(event);
+};
+
+UserDatabase.prototype.getEvents = function (params) {
+  params.query.push({ type: 'equal', content: { field: 'deleted', value: null } });
+  params.query.push({ type: 'equal', content: { field: 'headId', value: null } });
+  const queryString = prepareEventsGetQuery(params);
+  this.logger.debug(`GET Events: ${queryString}`);
+  const res = this.db.prepare(queryString).all();
+  if (res != null) {
+    return res.map(eventsSchema.fromDB);
   }
+  return null;
+};
 
-  getEventsDeletionsStream (deletedSince) {
-    this.logger.debug(`GET Events Deletions since: ${deletedSince}`);
-    return this.readableEventsStreamForIterator(this.get.eventsDeletedSince.iterate(deletedSince));
-  }
+UserDatabase.prototype.getEventsStream = function (params) {
+  params.query.push({ type: 'equal', content: { field: 'deleted', value: null } });
+  params.query.push({ type: 'equal', content: { field: 'headId', value: null } });
+  const queryString = prepareEventsGetQuery(params);
+  this.logger.debug(`GET Events Stream: ${queryString}`);
+  const query = this.db.prepare(queryString);
+  return this.readableEventsStreamForIterator(query.iterate());
+};
 
-  getEventsHistory (eventId) {
-    this.logger.debug(`GET Events History for: ${eventId}`);
-    return this.get.eventHistory.all(eventId).map(eventsSchema.fromDBHistory);
-  }
+UserDatabase.prototype.getEventsDeletionsStream = function (deletedSince) {
+  this.logger.debug(`GET Events Deletions since: ${deletedSince}`);
+  return this.readableEventsStreamForIterator(this.get.eventsDeletedSince.iterate(deletedSince));
+};
 
-  // also see: https://nodejs.org/api/stream.html#stream_stream_readable_from_iterable_options
-  readableEventsStreamForIterator (iterateSource) {
-    const iterateTransform = {
-      next: function () {
-        const res = iterateSource.next();
-        if (res && res.value) {
-          res.value = eventsSchema.fromDB(res.value);
-        }
-        return res;
+UserDatabase.prototype.getEventsHistory = function (eventId) {
+  this.logger.debug(`GET Events History for: ${eventId}`);
+  return this.get.eventHistory.all(eventId).map(eventsSchema.fromDBHistory);
+};
+
+// also see: https://nodejs.org/api/stream.html#stream_stream_readable_from_iterable_options
+UserDatabase.prototype.readableEventsStreamForIterator = function (iterateSource) {
+  const iterateTransform = {
+    next: function () {
+      const res = iterateSource.next();
+      if (res && res.value) {
+        res.value = eventsSchema.fromDB(res.value);
       }
-    };
+      return res;
+    }
+  };
 
-    iterateTransform[Symbol.iterator] = function () {
-      return iterateTransform;
-    };
+  iterateTransform[Symbol.iterator] = function () {
+    return iterateTransform;
+  };
 
-    return Readable.from(iterateTransform);
-  }
+  return Readable.from(iterateTransform);
+};
 
-  eventsCount () {
-    const res = this.db.prepare('SELECT count(*) as count FROM events').get();
-    return res?.count || 0;
-  }
+UserDatabase.prototype.eventsCount = function () {
+  const res = this.db.prepare('SELECT count(*) as count FROM events').get();
+  return res?.count || 0;
+};
 
-  close () {
-    this.db.close();
-  }
-}
+UserDatabase.prototype.close = function () {
+  this.db.close();
+};
+
+// -----------------------------------------------------------------------------
 
 function prepareEventsDeleteQuery (params) {
   if (params.streams) { throw new Error(`Events DELETE with stream query not supported yet: ${JSON.stringify(params)}`); }
@@ -343,5 +324,3 @@ function prepareQuery (params = {}, isDelete = false) {
   }
   return queryString;
 }
-
-module.exports = UserDatabase;
