be4f38c431892b2aa70585ff84132feb64f28667
Perki <perki@pryv.com>
2023-07-06 17:53:07 +0200

    adding hfs docker local test

--- build/test/start.sh
+++ build/test/start.sh
@@ -8,5 +8,14 @@ mkdir -p ${PRYV_CONF_ROOT}/pryv/mongodb/log
 mkdir -p ${PRYV_CONF_ROOT}/pryv/mongodb/data
 mkdir -p ${PRYV_CONF_ROOT}/pryv/core/log
 mkdir -p ${PRYV_CONF_ROOT}/pryv/core/data
+mkdir -p ${PRYV_CONF_ROOT}/pryv/hfs/log
+mkdir -p ${PRYV_CONF_ROOT}/pryv/influxdb/log
+mkdir -p ${PRYV_CONF_ROOT}/pryv/influxdb/data
+sudo chown -R 9999:9999 ${PRYV_CONF_ROOT}/pryv/hfs/log \
+${PRYV_CONF_ROOT}/pryv/influxdb/data \
+${PRYV_CONF_ROOT}/pryv/core/data \
+${PRYV_CONF_ROOT}/pryv/influxdb/log \
+${PRYV_CONF_ROOT}/pryv/core/log
+
 
 HOSTNAME=l.rec.la docker-compose -f ${PRYV_CONF_ROOT}/pryv.yml up
