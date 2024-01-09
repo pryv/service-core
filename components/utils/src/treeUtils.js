393172aba85b05ce6204005bfdc58a8ef2f1ec14
Perki <perki@pryv.com>
2024-01-09 10:28:01 +0100

    Updating license

--- components/utils/src/treeUtils.js
+++ components/utils/src/treeUtils.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-prototype-builtins */
 /**
  * @license
- * Copyright (C) 2012–2023 Pryv S.A. https://pryv.com - All Rights Reserved
+ * Copyright (C) 2012–2024 Pryv S.A. https://pryv.com - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
